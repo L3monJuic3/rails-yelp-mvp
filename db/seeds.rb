30c944b4a0415c1b6d5f6b43ff199363b783ddec
ethan lane <ethan.lane@outlook.co.nz>
2022-08-25 10:54:10 +0100

    Validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.time Restaurant.new(name: 'Dishoom', category: 'Indian')
+
